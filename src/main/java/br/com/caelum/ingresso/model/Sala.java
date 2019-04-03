0c8c52d8fa4e4054d731de5998431747258e8291
Ana Losnak <ana.losnak@caelum.com.br>
2019-04-02 23:26:22 -0300

    Pegando lugares da sala sem repetição

--- src/main/java/br/com/caelum/ingresso/model/Sala.java
+++ src/main/java/br/com/caelum/ingresso/model/Sala.java
@@ -2,11 +2,12 @@ package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
@@ -31,7 +32,7 @@ public class Sala {
 	private String nome;
 
 	@OneToMany(fetch = FetchType.EAGER)
-	private List<Lugar> lugares = new ArrayList<>();
+	private Set<Lugar> lugares = new HashSet<>();
 
 	private BigDecimal preco = new BigDecimal("0.0");
 
@@ -65,26 +66,18 @@ public class Sala {
 	}
 
 	public void add(Lugar lugar) {
-		this.lugares.add(lugar);
-	}
-
-	public List<Lugar> getLugares() {
-		return lugares;
-	}
-
-	public void setLugares(List<Lugar> lugares) {
-		this.lugares = lugares;
+		this.getLugares().add(lugar);
 	}
 
 	public Map<String, List<Lugar>> getMapaDeLugares() {
-		if (!this.lugares.isEmpty()) {
-			return this.lugares.stream().collect(Collectors.groupingBy(Lugar::getFileira, Collectors.toList()));
+		if (!this.getLugares().isEmpty()) {
+			return this.getLugares().stream().collect(Collectors.groupingBy(Lugar::getFileira, Collectors.toList()));
 		}
 		return Collections.emptyMap();
 	}
 
 	public Integer lugar(String fileira, Integer posicao) {
-		Optional<Lugar> optional = this.lugares.stream()
+		Optional<Lugar> optional = this.getLugares().stream()
 				.filter((x) -> fileira.equals(x.getFileira()) && posicao.equals(x.getPosicao())).findFirst();
 		return optional.get().getId();
 	}
@@ -95,5 +88,13 @@ public class Sala {
 
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
+	}
+
+	public Set<Lugar> getLugares() {
+		return lugares;
+	}
+
+	public void setLugares(Set<Lugar> lugares) {
+		this.lugares = lugares;
 	}
 }
