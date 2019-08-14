07479ad7d90bc9d70c58c720d7fa7aa0f7948f8a
Ana Losnak <ana.losnak@caelum.com.br>
2019-08-14 12:20:14 -0300

    Testando GerenciadorDeSessao

---
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -0,0 +1,44 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadorDeSessaoTest {
+	
+	private Filme filme;
+	private Sala sala;
+	private LocalTime horario;
+	private Sessao sessao;
+
+	@Before
+	public void preparaCenario() {
+		filme = new Filme("Rei Leao", Duration.ofMinutes(60), "SCI-FI");
+		sala = new Sala("Sala 1");
+		horario = LocalTime.parse("11:00:00");
+		sessao = new Sessao(horario, sala, filme);
+	}
+	
+	@Test
+	public void sessaoDeveCaberSeListaEstiverVazia() {
+		//prepara cenario
+		List<Sessao> sessoes = Collections.emptyList();	
+		
+		// chama a lógica
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		boolean resultado = gerenciador.cabe(sessao);
+		
+		// checar se resultado é o esperado
+		Assert.assertTrue(resultado);
+	}
+
+}
