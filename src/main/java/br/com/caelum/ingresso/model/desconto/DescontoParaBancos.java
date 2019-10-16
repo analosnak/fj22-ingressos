eb5c2b7fd573f7e9b216d0d9c87e79954b7b04f1
Ana Losnak <ana.losnak@caelum.com.br>
2019-10-16 14:36:24 -0300

    Preparando os descontos e ingresso

---
+++ src/main/java/br/com/caelum/ingresso/model/desconto/DescontoParaBancos.java
@@ -0,0 +1,12 @@
+package br.com.caelum.ingresso.model.desconto;
+
+import java.math.BigDecimal;
+
+public class DescontoParaBancos implements Desconto {
+
+	@Override
+	public BigDecimal aplicaDesconto(BigDecimal precoOriginal) {
+		return precoOriginal.multiply(new BigDecimal("0.7"));
+	}
+
+}
